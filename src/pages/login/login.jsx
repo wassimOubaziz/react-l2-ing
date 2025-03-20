f373029d147d550833903e953615a9eecef29607
Wassim Oubaziz <wassimdjaafar5@gmail.com>
2025-03-20 10:20:35 +0100

    Enhance login functionality with email and password state management

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import './login.css';
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        console.log('Email: '+ email + ' Password: ' + password);
+        //now lets send this data to the server
+    }
+
+    
+
   return (
     <div className='login'>
         <div className='box'>
@@ -10,13 +21,17 @@ export default function Login() {
             </div>
                 <div className='inputBox'>
                     <label>Email</label>
-                    <input type='email' placeholder='e.g. sample@example.com' />
+                    <input type='email' placeholder='e.g. sample@example.com' onChange={(e)=>{
+                        setEmail(e.target.value)
+                    }} />
                 </div>
                 <div className='inputBox'>
                     <label>Password</label>
-                    <input type='password' placeholder='••••••••' />
+                    <input type='password' placeholder='••••••••' onChange={(e)=>{
+                        setPassword(e.target.value)
+                    }}/>
                 </div>
-                <button className='login-button'>Login</button>
+                <button className='login-button' onClick={handleLogin}>Login</button>
                 <p className='have'>Don’t Have An Account? <a href='#' style={{color: "#1823F0", textDecoration: 'none'}}>Sign Up</a></p>
             </div>
     </div>
